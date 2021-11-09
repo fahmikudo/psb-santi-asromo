e849a50fa37af80fb0f91a28f680ba7aaa51257e
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2021-11-09 17:22:57 +0700

    initiate project psb online v2

---
+++ h312ud1/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+	$host = 'localhost';
+	$username = 'herudi_root';
+	$password = 'backdoor1@#';
+	$database = 'herudi_psbsanti_new';
+	$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+?>
